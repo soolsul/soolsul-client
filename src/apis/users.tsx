3a2ae0e79300e9099acf3988f7136b4054730ab8
soonki-98 <alstnsrl98@gmail.com>
2022-10-24 23:05:17 +0900

    feat: 개발 환경 세팅 #1

---
+++ src/apis/users.tsx
@@ -0,0 +1,17 @@
+import APIS from "./network";
+
+/**
+ * 로그인하기
+ * @param id 유저 아이디
+ * @param password 비밀번호
+ */
+export function login(data: { id: string; password: string }) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await APIS.user.login(data);
+      resolve(result);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
