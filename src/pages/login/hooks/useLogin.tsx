ac5e99072d3f0f5fc0d67fca795f9925e8a396ed
soonki-98 <alstnsrl98@gmail.com>
2022-10-27 22:10:49 +0900

    feat: onChange 함수 추가 #1

--- src/pages/login/hooks/useLogin.tsx
+++ src/pages/login/hooks/useLogin.tsx
@@ -1,15 +1,26 @@
 import apis from "@apis/index";
-import { LoginType } from "@lib/types";
-interface IUseLoginType extends LoginType.FormData {}
+import { LoginType } from "@lib/types/user";
+import { useState } from "react";
 
-export default function useLogin(data: IUseLoginType) {
+export default function useLogin() {
+  const [loginInfo, setLoginInfo] = useState<LoginType>({ id: "", password: "" });
+
+  const handleChangeLoginInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.id === "ID") {
+      setLoginInfo({ ...loginInfo, id: e.target.value });
+    } else {
+      setLoginInfo({ ...loginInfo, password: e.target.value });
+    }
+  };
+
   const handleLoginSubmit = async () => {
     try {
-      await apis.user.login(data);
+      await apis.user.login(loginInfo);
+      setLoginInfo({ id: "", password: "" });
     } catch (err) {
       console.error(err);
     }
   };
 
-  return { handleLoginSubmit };
+  return { loginInfo, handleChangeLoginInfo, handleLoginSubmit };
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,24 +1,17 @@
-import { LoginType } from "@lib/types";
-import React, { useState } from "react";
+import React from "react";
 import useLogin from "./hooks/useLogin";
 
-export default function Login() {
-  const [loginInfo, setLoginInfo] = useState<LoginType.FormData>({ id: "", password: "" });
-  const { handleLoginSubmit } = useLogin(loginInfo);
-
-  const handleChangeLoginInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.id === "ID") {
-      setLoginInfo({ ...loginInfo, id: e.target.value });
-    } else {
-      setLoginInfo({ ...loginInfo, password: e.target.value });
-    }
-  };
+function Login() {
+  const { loginInfo, handleChangeLoginInfo, handleLoginSubmit } = useLogin();
+  const { id, password } = loginInfo;
 
   return (
-    <>
-      <input id="ID" value={loginInfo.id} placeholder="ID" onChange={handleChangeLoginInfo} />
-      <input id="PASSWORD" value={loginInfo.password} placeholder="PASSWORD" onChange={handleChangeLoginInfo} />
+    <div>
+      <input id="ID" type="text" value={id} placeholder="ID" onChange={handleChangeLoginInfo} />
+      <input id="PASSWORD" type="password" value={password} placeholder="PASSWORD" onChange={handleChangeLoginInfo} />
       <button onClick={handleLoginSubmit}>LOGIN</button>
-    </>
+    </div>
   );
 }
+
+export default Login;
