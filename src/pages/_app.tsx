d9d1f2951fb7a39f9bdb621df65bdc1c00517beb
soonki-98 <alstnsrl98@gmail.com>
2022-10-31 22:16:49 +0900

    [Clean] 필요없는 코드 제거 #1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import { useEffect } from "react";
-import Property from "@lib/utils/Properties";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    (async () => {
-      console.log(await Property.userInfo.currentPosition());
-    })();
-  }, [Property.userInfo.currentPosition()]);
   return (
     <RecoilRoot>
       <Component {...pageProps} />
