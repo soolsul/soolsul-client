0f345ce794b2a60d3f1437a7670e366a71bdc261
soonki-98 <alstnsrl98@gmail.com>
2022-10-31 22:16:25 +0900

    [Feat] 버튼 클릭시 위치 정보 나오는 예제 #1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import Property from "@lib/utils/Properties";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div />;
+  return (
+    <div>
+      <button
+        onClick={async () => {
+          console.log("클릭 ", await Property.userInfo.currentPosition());
+        }}
+      >
+        위치정보 가져오기
+      </button>
+    </div>
+  );
 };
 
 export default Home;
