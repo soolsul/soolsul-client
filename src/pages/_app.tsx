dc739ae90712fcf6a21adf9118bbb36f5e53314c
soonki-98 <alstnsrl98@gmail.com>
2022-11-03 00:33:03 +0900

    [Setting]: 카카오 지도 API 관련 세팅 #5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Script
+        src="//dapi.kakao.com/v2/maps/sdk.js?appkey=f16ce2c53cddfe4e24c640dde411dca5&libraries=services,clusterer&autoload=false"
+        strategy="beforeInteractive"
+      />
       <Component {...pageProps} />
     </RecoilRoot>
   );
