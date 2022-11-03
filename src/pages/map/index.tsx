f93952dc202c0266ae4f750c827a409db13b64a1
soonki-98 <alstnsrl98@gmail.com>
2022-11-04 00:10:05 +0900

    [Feat] 지도 화면에 하단바 구현 #5

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -2,28 +2,31 @@ import Property from "@lib/utils/Properties";
 import dynamic from "next/dynamic";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import styled from "styled-components";
-import { Error, SearchBar } from "./components";
+import { BottomMenu, Error, SearchBar } from "./components";
 
 function MapPage() {
   try {
     const location: GeolocationPosition = Property.userInfo.location;
     return (
-      <StyledMap
-        center={{
-          lat: location.coords.latitude,
-          lng: location.coords.longitude,
-        }}
-      >
-        <SearchBar placeholder="장소를 검색해 보세요" value="" />
-        <MapMarker
-          position={{
+      <Wrapper>
+        <StyledMap
+          center={{
             lat: location.coords.latitude,
             lng: location.coords.longitude,
           }}
         >
-          <div style={{ color: "#000" }}>현위치</div>
-        </MapMarker>
-      </StyledMap>
+          <SearchBar placeholder="장소를 검색해 보세요" value="" />
+          <MapMarker
+            position={{
+              lat: location.coords.latitude,
+              lng: location.coords.longitude,
+            }}
+          >
+            <div style={{ color: "#000" }}>현위치</div>
+          </MapMarker>
+          <BottomMenu />
+        </StyledMap>
+      </Wrapper>
     );
   } catch (error) {
     return <Error error={error} />;
@@ -32,11 +35,16 @@ function MapPage() {
 
 export default dynamic(() => Promise.resolve(MapPage), { ssr: false });
 
-const StyledMap = styled(Map)`
-  width: 100%;
-  height: 100vh;
-  max-width: 480px;
-  position: fixed;
+const Wrapper = styled.div`
   left: 50%;
   transform: translateX(-50%);
+  max-width: 480px;
+  position: relative;
+  overflow: hidden;
+  height: 100vh;
+`;
+
+const StyledMap = styled(Map)`
+  height: 100%;
+  width: 100%;
 `;
