5df935f90a5fca124d1ace921441ed4b1c415dcd
soonki-98 <alstnsrl98@gmail.com>
2022-11-04 23:12:48 +0900

    [Feat] _document.tsx 파일 생성 #5
    웹 접근성이나 스크립트 관련 작업은 해당 파일에서 해야함

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Script
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services,clusterer&autoload=false`}
-        strategy="beforeInteractive"
-      />
       <Component {...pageProps} />
     </RecoilRoot>
   );
