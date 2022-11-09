3d64f40079ee4bfd6c2de9b67ed592c1599f7695
soonki-98 <alstnsrl98@gmail.com>
2022-11-10 01:30:29 +0900

    [Feat] MyPage 생성 #6

---
+++ src/pages/mypage/index.tsx
@@ -0,0 +1,11 @@
+import { BottomMenu, CommonWrapper } from "@components/pages/common";
+
+function MyPage() {
+  return (
+    <CommonWrapper>
+      <BottomMenu />
+    </CommonWrapper>
+  );
+}
+
+export default MyPage;
