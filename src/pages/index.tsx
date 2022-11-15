838ed267ae88fbb8abb2ed22e90c834b995c6dee
Yunah <favoryun12@gmail.com>
2022-11-16 00:29:13 +0900

    [Feat] 앱 시작시 랜딩 페이지 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,69 @@
-import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { CommonWrapper } from '@components/common'
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import styled from 'styled-components'
 
 const Home: NextPage = () => {
-  const router = useRouter();
+  const router = useRouter()
   useEffect(() => {
-    if (typeof window === "undefined") return;
-    window.localStorage.removeItem("GeoLocation");
-  }, []);
+    if (typeof window === 'undefined') return
+    window.localStorage.removeItem('GeoLocation')
+  }, [])
   return (
-    <div>
-      <button
-        style={{ display: "block" }}
-        onClick={() => {
-          router.push("map");
-        }}
-      >
-        입장하기
-      </button>
-    </div>
-  );
-};
+    <Wrapper>
+      <HomeContainer>
+        <div className='logo'>LOGO</div>
+        <button
+          style={{ display: 'block' }}
+          onClick={() => {
+            router.push('map')
+          }}
+        >
+          게스트로 들어가기
+        </button>
+        <button
+          style={{ display: 'block' }}
+          onClick={() => {
+            router.push('login')
+          }}
+        >
+          로그인
+        </button>
+      </HomeContainer>
+    </Wrapper>
+  )
+}
 
-export default Home;
+export default Home
+
+const Wrapper = styled(CommonWrapper)`
+  background-color: #fff;
+`
+
+const HomeContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .logo {
+    margin: 3rem;
+    min-width: 13rem;
+    min-height: 13rem;
+    background: #e6e6e6;
+    text-align: center;
+  }
+
+  button {
+    width: 75%;
+    padding: 15px 25px;
+    margin: 10px;
+    border-radius: 25px;
+    border: none;
+    background: #5f3dc4;
+    color: #fff;
+    font-weight: 700;
+  }
+`
