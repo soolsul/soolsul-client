70488f7b4b0b852ad8f9cbf1c052c33bc362d8c7
soonki-98 <alstnsrl98@gmail.com>
2022-11-26 23:34:04 +0900

    [Refactor] API 인ㅅ스턴스 리팩터링 #27

--- src/apis/bar.ts
+++ src/apis/bar.ts
@@ -1,21 +1,18 @@
 import { BarType } from '@lib/types';
-import { AxiosResponse } from 'axios';
-import APIS from './network';
+import instance from './instance';
 
-/**
- * 로그인하기
- * @param id 유저 아이디
- * @param password 비밀번호
- */
-export function getBarList(data: BarType.barSearchTyoe): Promise<{ code: string; data: any; message: string }> {
-  return new Promise((resolve, reject) => {
+class BarAPI {
+  public async getBarList(data: BarType.barSearchTyoe) {
     try {
-      (async () => {
-        const result: AxiosResponse<{ code: string; data: any; message: string }> = await APIS.bar.getBarList(data);
-        resolve(result.data);
-      })();
+      const { latitude, longitude, zoomLevel, moodTag, drinkTag } = data;
+      const result = await instance.get(
+        `/api/bars?latitude=${latitude}&longitude=${longitude}&level=${zoomLevel}&barMoodTagNames=${moodTag}&barAlcoholTagNames=${drinkTag}`
+      );
+      return result.data;
     } catch (error) {
-      reject(error);
+      console.log(error);
     }
-  });
+  }
 }
+
+export default new BarAPI();
