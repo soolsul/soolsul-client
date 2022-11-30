f476607549ea9df1faffb2652b4181f45cd4ef90
soonki-98 <alstnsrl98@gmail.com>
2022-11-30 20:24:09 +0900

    [Fix] barList 조회 api default param 수정 #32

--- src/apis/bar.ts
+++ src/apis/bar.ts
@@ -1,19 +1,18 @@
 import { BarType } from '@lib/types';
 import RestAPI from './restapi';
 
-
 class BarAPI {
-/**
- * 술집 목록 조회
- * @param latitude
- * @param longitude
- * @param zoomLevel
- * @param moodTag
- * @param drinkTag
- */
+  /**
+   * 술집 목록 조회
+   * @param latitude
+   * @param longitude
+   * @param zoomLevel
+   * @param moodTag
+   * @param drinkTag
+   */
   public async getBarList(data: BarType.barSearchTyoe) {
     try {
-      const { latitude, longitude, zoomLevel, moodTag, drinkTag } = data;
+      const { latitude, longitude, zoomLevel = 6, moodTag = '', drinkTag = '' } = data;
       const result = await RestAPI.get(
         `/api/bars?latitude=${latitude}&longitude=${longitude}&level=${zoomLevel}&barMoodTagNames=${moodTag}&barAlcoholTagNames=${drinkTag}`
       );
