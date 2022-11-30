2f89bc58fb02bcbdfc9afe64f56b5692db1b4fc7
soonki-98 <alstnsrl98@gmail.com>
2022-12-01 00:22:11 +0900

    [Feat] 공통 Shadow 컴포넌트 생성

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
-export { default as BottomMenu } from './BottomMenu'
-export { default as Line } from './Line'
-export { default as Header } from './Header'
-export { default as ProfileImage } from './ProfileImage'
-export * from './commonStyle'
+export { default as BottomMenu } from './BottomMenu';
+export { default as Line } from './Line';
+export { default as Header } from './Header';
+export { default as ProfileImage } from './ProfileImage';
+export { default as Shadow } from './Shadow';
+export * from './commonStyle';
